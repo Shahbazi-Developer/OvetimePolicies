bb44a07d228e28a1977eeed4615491a6712b9249
p.shahbazi <pouyash.dev@gmail.com>
2026-05-16 12:31:15 +0330

    Add project files.

---
+++ src/2.Infra/Data/Sql.Commands/Common/OvetimePoliciesCommandDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OvetimePolicies.Infra.Data.Sql.Commands.Common;
+
+public class OvetimePoliciesCommandDbContextFactory : IDesignTimeDbContextFactory<OvetimePoliciesCommandDbContext>
+{
+    public OvetimePoliciesCommandDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<OvetimePoliciesCommandDbContext>();
+
+        builder.UseSqlServer("Server =.; Database=OvetimePoliciesDb;User Id = ;Password = ; MultipleActiveResultSets = true; Encrypt = false");
+
+        return new OvetimePoliciesCommandDbContext(builder.Options);
+    }
+}
---
+++ src/2.Infra/Data/Sql.Queries/Common/OvetimePoliciesQueryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zamin.Infra.Data.Sql.Queries;
+
+namespace OvetimePolicies.Infra.Data.Sql.Queries.Common;
+
+public class OvetimePoliciesQueryDbContext : BaseQueryDbContext
+{
+    public OvetimePoliciesQueryDbContext(DbContextOptions<OvetimePoliciesQueryDbContext> options) : base(options)
+    {
+    }
+}
