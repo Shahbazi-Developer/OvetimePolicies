545bfec378bd1254ad753ac51b64b06b3b478a2a
p.shahbazi <pouyash.dev@gmail.com>
2026-05-16 12:31:33 +0330

    Revert "Add project files."
    This reverts commit bb44a07d228e28a1977eeed4615491a6712b9249.

--- _inspect_types/Program.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-foreach (var name in args)
-{
-    Assembly a = Assembly.LoadFrom(name);
-    Console.WriteLine("=== " + a.GetName()?.Name + " ===");
-    foreach (var t in a.GetExportedTypes().OrderBy(t => t.FullName))
-        Console.WriteLine(t.FullName);
-}
